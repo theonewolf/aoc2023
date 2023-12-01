40a8f615df195dd027c0d4cdbb30e0a48e40bf62
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-01 18:57:13 -0500

    1: solved Day 1, Part 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,3 +1,22 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+// Part 1
+
+List<int> nums = new();
+
+foreach (string line in System.IO.File.ReadLines(@"./input")) {
+    int first = -1;
+    int last = -1;
+
+    foreach (char c in line) {
+        if (char.IsDigit(c)) {
+            if (first == -1) {
+                first = int.Parse(c.ToString());
+            }
+            last = int.Parse(c.ToString());
+        }
+    }
+
+    nums.Add(first * 10 + last);
 }
+
+Console.WriteLine(string.Join(" ", nums));
+Console.WriteLine(nums.Sum());
