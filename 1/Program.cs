ab6bcce1ca5e991195d8cfdcc8c1477cc27bd130
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-01 19:10:33 -0500

    day 1, part 2

--- 1/Program.cs
+++ 1/Program.cs
@@ -18,5 +18,47 @@ foreach (string line in System.IO.File.ReadLines(@"./input")) {
     nums.Add(first * 10 + last);
 }
 
-Console.WriteLine(string.Join(" ", nums));
 Console.WriteLine(nums.Sum());
+
+// Part 2
+List<int> nums2 = new();
+
+foreach (string line in System.IO.File.ReadLines(@"./input")) {
+    int first = -1;
+    int last = -1;
+
+    for (int i = 0; i < line.Length; i++) {
+        if (char.IsDigit(line[i])) {
+            if (first == -1) {
+                first = int.Parse(line[i].ToString());
+            }
+            last = int.Parse(line[i].ToString());
+            continue;
+        }
+
+        int number = line[i..] switch
+        {
+            string s when s.StartsWith("one") => 1,
+            string s when s.StartsWith("two") => 2,
+            string s when s.StartsWith("three") => 3,
+            string s when s.StartsWith("four") => 4,
+            string s when s.StartsWith("five") => 5,
+            string s when s.StartsWith("six") => 6,
+            string s when s.StartsWith("seven") => 7,
+            string s when s.StartsWith("eight") => 8,
+            string s when s.StartsWith("nine") => 9,
+            _ => -1
+        };
+
+        if (number != -1) {
+            if (first == -1) {
+                first = number;
+            }
+            last = number;
+        }
+    }
+
+    nums2.Add(first * 10 + last);
+}
+
+Console.WriteLine(nums2.Sum());
