905da3ce362e4dacd87488a5436866f7bdc0d9d2
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-03 06:33:16 -0500

    day 3 partly solved

---
+++ 3/Grid.cs
@@ -0,0 +1,65 @@
+public class GridNumber
+{
+    public int Col { get; private set; }
+    public int Row { get; private set; }
+    public int Width { get; private set; }
+    public string Data { get; private set; }
+    public int Value { get; private set; }
+
+    public GridNumber(int col, int row, int width, string data, int value)
+    {
+        Col = col;
+        Row = row;
+        Width = width;
+        Data = data;
+        Value = value;
+    }
+
+    public bool Contains(int row, int col) {
+        return col >= Col &&
+            col < Col + Width &&
+            row == Row;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is GridNumber other)
+        {
+            return Col == other.Col && Row == other.Row && Width == other.Width && Value == other.Value;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Col, Row, Width, Data, Value);
+    }
+}
+
+public class Symbol
+{
+   public int Col { get; private set; }
+   public int Row { get; private set; }
+   public string Data { get; private set; }
+
+   public Symbol(int col, int row, string data)
+   {
+       Col = col;
+       Row = row;
+       Data = data;
+   }
+
+   public override bool Equals(object obj)
+   {
+       if (obj is Symbol other)
+       {
+           return Col == other.Col && Row == other.Row && Data == other.Data;
+       }
+       return false;
+   }
+
+   public override int GetHashCode()
+   {
+       return HashCode.Combine(Col, Row, Data);
+   }
+}
--- 3/Program.cs
+++ 3/Program.cs
@@ -1,3 +1,103 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+// Part 1
+
+// Parse all grid numbers + symbols
+HashSet<GridNumber> gridNumbers = new();
+List<Symbol> symbols = new();
+
+int row_index = 0;
+foreach (string row in System.IO.File.ReadLines(@"./input")) {
+
+    int x = -1;
+    int y = -1;
+    string data = "";
+
+    int col_index = 0;
+    // For each col in row
+    foreach (var col in row) {
+        switch (col) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (x < 0) {
+                    x = col_index;
+                }
+
+                if (y < 0) {
+                    y = row_index;
+                }
+
+                data += col;
+                break;
+            default:
+                if (data != "") {
+                    gridNumbers.Add(new GridNumber(x, y, data.Length, data, int.Parse(data)));
+                    x = -1;
+                    y = -1;
+                    data = "";
+                }
+
+                if (col != '.') symbols.Add(new Symbol(col_index, row_index, col.ToString()));
+                break;
+        }
+
+        col_index++;
+    }
+
+    row_index++;
 }
+
+// Check for all symbols touching GridNumbers
+HashSet<GridNumber> symbolsTouched = new();
+
+foreach (var symbol in symbols) {
+    foreach (var number in gridNumbers) {
+        // North
+        if (number.Contains(symbol.Row, symbol.Col - 1)) {
+            symbolsTouched.Add(number);
+        }
+
+        // North-East
+        if (number.Contains(symbol.Row + 1, symbol.Col - 1)) {
+            symbolsTouched.Add(number);
+        }
+
+        // East
+        if (number.Contains(symbol.Row + 1, symbol.Col)) {
+            symbolsTouched.Add(number);
+        }
+
+        // South-East
+        if (number.Contains(symbol.Row + 1, symbol.Col + 1)) {
+            symbolsTouched.Add(number);
+        }
+
+        // South
+        if (number.Contains(symbol.Row, symbol.Col + 1)) {
+            symbolsTouched.Add(number);
+        }
+
+        // South-West
+        if (number.Contains(symbol.Row - 1, symbol.Col + 1)) {
+            symbolsTouched.Add(number);
+        }
+
+        // West
+        if (number.Contains(symbol.Row - 1, symbol.Col)) {
+            symbolsTouched.Add(number);
+        }
+
+        // North-West
+        if (number.Contains(symbol.Row - 1, symbol.Col - 1)) {
+            symbolsTouched.Add(number);
+        }
+    }
+}
+
+Console.WriteLine(symbolsTouched.Select(x => x.Value).Sum());
