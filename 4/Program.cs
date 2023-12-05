0fb639add59bd5441ead859b27a3e1ff14f36931
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-04 21:25:43 -0500

    day 4 part 1

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,3 +1,28 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+// Part 1
+
+List<long> card_values = new();
+
+foreach (string line in System.IO.File.ReadLines(@"./input")) {
+    var data = line.Split(":")[1];
+    var numbers =  data.Split("|");
+
+    var winning_numbers = new HashSet<long>(numbers[0].Split(" ", options: StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)));
+    var my_numbers = new HashSet<long>(numbers[1].Split(" ", options: StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)));
+    
+    int winning_counter = 0;
+    int score = 0;
+    
+    foreach (var number in my_numbers) {
+        if (winning_numbers.Contains(number)) {
+            winning_counter++;
+            score = score == 0 ? 1 : score * 2;
+        }
+    }
+
+    card_values.Add(score);
 }
+
+Console.WriteLine(card_values.Sum());
+
+// Part 2
+
