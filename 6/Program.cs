a7702d15dcb87bc923883843358fdab0b928aaff
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-06 20:29:42 -0500

    day 6 part 1

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,3 +1,24 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+// Part 1
+
+string[] lines = System.IO.File.ReadLines(@"./input").ToArray();
+long[] times = lines[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray();
+long[] distances = lines[1].Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray();
+
+var zipped = times.Zip(distances, (time, distance) => ( time, distance ));
+List<long> total_win_methods = new();
+
+foreach ((long time, long distance) in zipped)
+{
+    long win_methods = 0;
+
+    for (long hold = 0; hold < time; hold++) {
+        long boat_distance = hold * (time - hold);
+        if (boat_distance > distance) {
+            win_methods += 1;
+        }
+    }
+
+    total_win_methods.Add(win_methods);
 }
+
+Console.WriteLine(total_win_methods.Aggregate(1L, (acc, val) => acc * val));
