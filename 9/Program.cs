ac8b4c560c41491d54c19c63a100072edf3446cc
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-10 15:28:59 -0500

    dau 9 part 2

--- 9/Program.cs
+++ 9/Program.cs
@@ -27,3 +27,27 @@ foreach (var sequence in sequences) {
 }
 
 Console.WriteLine(predictions.Sum());
+
+// Part 2
+
+List<long> predictions2 = [];
+foreach (var sequence in sequences) {
+    List<List<long>> rounds = [sequence];
+
+    // Easy bug here if you check for sum to 0, it is possible that a sequence of positive and negative
+    // integers sum to 0 --> but all diffs are not 0 yet.
+    while (!rounds.Last().All(x => x == 0)) {
+        rounds.Add(rounds.Last().Zip(rounds.Last().Skip(1), (x, y) => y - x).ToList());
+    }
+
+    rounds.Reverse();
+    long curVal = 0;
+
+    foreach (var round in rounds) {
+        curVal = round.First() - curVal;
+    }
+
+    predictions2.Add(curVal);
+}
+
+Console.WriteLine(predictions2.Sum());
