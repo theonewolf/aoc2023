1bb83797bb38579aa13704d2344468d30d6d87df
Wolfgang Richter <wolf@wolfgroup.llc>
2023-12-09 20:44:00 -0500

    day 8 part 2 attempt

--- 8/Program.cs
+++ 8/Program.cs
@@ -22,6 +22,7 @@ foreach (string line in lines[2..]) {
 }
 
 long stepCounter = 0;
+
 Node<string> current = cache["AAA"];
 
 while (current.Value != "ZZZ") {
@@ -31,6 +32,25 @@ while (current.Value != "ZZZ") {
         'R' => cache[current.Right.Value],
         _ => throw new InvalidDataException(),
     };
+    stepCounter++;
+}
+
+Console.WriteLine(stepCounter);
+
+// Part 2 -- need to redo
+
+List<Node<string>> current_nodes = cache.Where(kv => kv.Key.EndsWith('A')).Select(kv => kv.Value).ToList();
+stepCounter = 0;
+
+while (!current_nodes.All(node => node.Value.EndsWith('Z'))) {
+    for (int i = 0; i < current_nodes.Count; i++) {
+        current_nodes[i] = directions[stepCounter % directions.Length] switch
+        {
+            'L' => cache[current_nodes[i].Left.Value],
+            'R' => cache[current_nodes[i].Right.Value],
+            _ => throw new InvalidDataException(),
+        };
+    }
     stepCounter++;
 }
 
